f0a7de218d496feb1ca96829dba749e7cd150765
tac-yacht <github@iyher.com>
2018-05-03 14:38:31 +0900

    add show hover

--- src/com/iyher/eclipse/kdoc/KDocHover.kt
+++ src/com/iyher/eclipse/kdoc/KDocHover.kt
@@ -5,17 +5,27 @@ import org.eclipse.jface.text.IInformationControlCreator;
 import org.jetbrains.kotlin.kdoc.lexer.KDocTokens;
 import org.jetbrains.kotlin.ui.editors.KotlinEditor;
 import org.jetbrains.kotlin.ui.editors.hover.HoverData;
+import com.intellij.lang.ASTNode
 
 class KDocHover : KotlinEditorTextHover<String> {
-	override fun getHoverInfo(hoverData: HoverData): String? {
-		return null
+	override fun isAvailable(hoverData: HoverData): Boolean {
+		var node = getDocNode(hoverData)
+		node?.let {
+			return true;
+		}
+		return false
 	}
 
-	override fun isAvailable(hoverData: HoverData): Boolean {
-		return false
+	override fun getHoverInfo(hoverData: HoverData): String? {
+		return "plugin output:" + hoverData.component1().getName()
 	}
 
 	override fun getHoverControlCreator(editor: KotlinEditor): IInformationControlCreator? {
 		return null
 	}
+
+	private fun getDocNode(hoverData: HoverData): ASTNode? {
+		return hoverData.component1().getNode().findChildByType(KDocTokens.KDOC)
+	}
+
 }
