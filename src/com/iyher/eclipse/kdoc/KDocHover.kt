a55034e079f4f2580734535b17dc771410cdf60a
tac-yacht <github@iyher.com>
2018-05-03 16:29:04 +0900

    show TextNode by plain format

--- src/com/iyher/eclipse/kdoc/KDocHover.kt
+++ src/com/iyher/eclipse/kdoc/KDocHover.kt
@@ -1,15 +1,17 @@
 package com.iyher.eclipse.kdoc
 
 import org.jetbrains.kotlin.ui.editors.hover.KotlinEditorTextHover
-import org.eclipse.jface.text.IInformationControlCreator;
-import org.jetbrains.kotlin.kdoc.lexer.KDocTokens;
-import org.jetbrains.kotlin.ui.editors.KotlinEditor;
-import org.jetbrains.kotlin.ui.editors.hover.HoverData;
+import org.eclipse.jface.text.IInformationControlCreator
+import org.jetbrains.kotlin.kdoc.lexer.KDocTokens
+import org.jetbrains.kotlin.ui.editors.KotlinEditor
+import org.jetbrains.kotlin.ui.editors.hover.HoverData
 import com.intellij.lang.ASTNode
+import com.intellij.psi.tree.TokenSet
+import org.jetbrains.kotlin.kdoc.parser.KDocElementTypes
 
 class KDocHover : KotlinEditorTextHover<String> {
 	override fun isAvailable(hoverData: HoverData): Boolean {
-		var node = getDocNode(hoverData)
+		val node = getDocNode(hoverData)
 		node?.let {
 			return true;
 		}
@@ -17,7 +19,18 @@ class KDocHover : KotlinEditorTextHover<String> {
 	}
 
 	override fun getHoverInfo(hoverData: HoverData): String? {
-		return "plugin output:" + hoverData.component1().getName()
+		val kDocRoot = getDocNode(hoverData)!!
+		//already checked by KotlinEditorTextHover
+
+		val kDocSection = kDocRoot.findChildByType(KDocElementTypes.KDOC_SECTION)
+		kDocSection?.let {
+			val kDocSectionChildren = kDocSection.getChildren(TokenSet.ANY)
+			val textNodes = kDocSectionChildren.filter({node -> KDocTokens.TEXT.equals(node.elementType)})
+
+			return hoverData.component1().getName() + "<br>" + textNodes.joinToString(separator = "", transform = ASTNode::getText)
+		}
+		return "plugin output: not found"
+
 	}
 
 	override fun getHoverControlCreator(editor: KotlinEditor): IInformationControlCreator? {
